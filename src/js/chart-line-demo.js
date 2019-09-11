1bc7e9c3224e07d205240ad66e70ae4a2beefe22
John Chase <ratteal@gmail.com>
2019-09-11 22:29:25 +0100

    Get the line chart working.

--- src/js/chart-line-demo.js
+++ src/js/chart-line-demo.js
@@ -2,29 +2,20 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-function number_format(number, decimals, dec_point, thousands_sep) {
-  // *     example: number_format(1234.56, 2, ',', ' ');
-  // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
-  var n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
-    toFixedFix = function(n, prec) {
-      var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-  }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec);
+this._seed = 1;
+
+
+// Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+function activity() {
+  return Math.round(rand(0, 100));
+}
+
+function rand(min, max) {
+  var seed = this._seed;
+  min = min === undefined ? 0 : min;
+  max = max === undefined ? 1 : max;
+  this._seed = (seed * 9301 + 49297) % 233280;
+  return min + (this._seed / 233280) * (max - min);
 }
 
 // Line Chart Example
@@ -32,87 +23,100 @@ var ctx = document.getElementById("myLineChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
     datasets: [{
-      label: "Earnings",
-      lineTension: 0.3,
+      label: "Nala",
+      backgroundColor: "rgb(75, 192, 192, 0.05)",
+      borderColor: "rgba(75, 192, 192, 1)",
+      data: [
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity()
+      ],
+      fill: "false"
+    }, {
+      label: "Marnie",
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
-      pointRadius: 3,
-      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointBorderColor: "rgba(78, 115, 223, 1)",
-      pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity(),
+        activity()
+      ],
+      fill: "false"
     }],
   },
   options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
+    responsive: true,
+    tooltips: {
+      mode: 'nearest',
+      intersect: false,
     },
     scales: {
       xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Hour'
         },
         ticks: {
-          maxTicksLimit: 7
-        }
+          maxTicksLimit: 24
+        },
       }],
       yAxes: [{
-        ticks: {
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Activity Level'
         },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
+        ticks: {
+          min: 0,
+          max: 100,
+          stepSize: 10
         }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
+      }]
     }
   }
 });
