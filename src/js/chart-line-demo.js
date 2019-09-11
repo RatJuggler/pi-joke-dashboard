f6212da725abc0f44d99234adfc39bdc43988fae
John Chase <ratteal@gmail.com>
2019-09-11 22:36:08 +0100

    Supply random seed.

--- src/js/chart-line-demo.js
+++ src/js/chart-line-demo.js
@@ -2,7 +2,7 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-this._seed = 1;
+this._seed = new Date().getTime();
 
 
 // Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
