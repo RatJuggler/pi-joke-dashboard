952a323019204ee30d230af6231ffc27add6f3a8
John Chase <ratteal@gmail.com>
2019-09-17 15:53:09 +0100

    Show some alerts.

--- src/js/dashboard-init.js
+++ src/js/dashboard-init.js
@@ -25,21 +25,56 @@ class Dinosaurs {
   }
 }
 
-dinosaurs = new Dinosaurs();
+let dinosaurs = new Dinosaurs();
 dinosaurs.populateFromFile("https://raw.githubusercontent.com/junosuarez/dinosaurs/master/dinosaurs.csv");
 
 function random(limit) {
   return Math.floor(Math.random() * limit)
 }
 
+let numberOfAlerts = 0;
+let alertContainer = $("#alertsContainer");
+const alertCount = '<span class="badge badge-danger badge-counter" id="alertsCounter">0</span>';
+const alertHeader = '<h6 class="dropdown-header">Alerts Center</h6>';
+const alertStart = '<a class="dropdown-item d-flex align-items-center" href="#"><div class="mr-3"><div class="icon-circle bg-danger"><i class="fas fa-exclamation-triangle text-white"></i></div></div><div>';
+const alertTime = '<div class="small text-gray-500">#TIME#</div>';
+const alertText = '<span class="font-weight-bold">#TEXT#</span>';
+const alertEnd = '</div></a>';
+const overAlerts = '<a class="dropdown-item text-center small text-danger" href="#">Too many alerts to show!</a>';
+
+function addAlert(description) {
+  if (numberOfAlerts === 0) {
+    alertContainer.html(alertHeader);
+    $('#alertsDropdown').append(alertCount)
+  }
+  numberOfAlerts++;
+  $('#alertsCounter').text(numberOfAlerts);
+  if (numberOfAlerts > 9) {
+    if (numberOfAlerts === 10) {
+      alertContainer.append(overAlerts);
+    }
+  } else {
+    let alertAt = alertTime.replace("#TIME#", new Date().toLocaleString());
+    let alertFor = alertText.replace("#TEXT#", description);
+    let newAlert = alertStart + alertAt + alertFor + alertEnd;
+    alertContainer.append(newAlert);
+  }
+}
+
 function pieChartUpdate() {
-  let frogs = 30 + random(20);
-  let toads = 15 + random(10);
-  let newts = 15 + random(10);
-  let sharks = 2 + random(5);
-  let crocodiles = 2 + random(5);
+  let frogs = 30 + random(21);
+  let toads = 15 + random(11);
+  let newts = 15 + random(11);
+  let sharks = random(8);
+  let crocodiles = random(8);
   pieChart.data.datasets[0].data = [frogs, toads, newts, sharks, crocodiles];
   pieChart.update();
+  if (sharks > 6) {
+    addAlert("More than 6 sharks seen!");
+  }
+  if (crocodiles > 6) {
+    addAlert("More than 6 crocodiles seen!")
+  }
 }
 
 setInterval(function () {
