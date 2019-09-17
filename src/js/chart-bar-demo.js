19544cdcfee87aef03a700d48e080ca091a68483
John Chase <ratteal@gmail.com>
2019-09-17 12:20:18 +0100

    Move to common init.

--- src/js/chart-bar-demo.js
+++ src/js/chart-bar-demo.js
@@ -1,7 +1,3 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
-
 // Bar Chart Example
 let grassHeight = Math.floor(10 + (Math.random() * 20));
 
--- src/js/chart-line-demo.js
+++ src/js/chart-line-demo.js
@@ -1,7 +1,3 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
-
 // Line Chart Example
 let ctxLineChart = document.getElementById("myLineChart");
 new Chart(ctxLineChart, {
